f69842e896a28b0b542b04d94b5e05fe3dbb35b9
dimir96 <dimir996@gmail.com>
2022-07-10 21:40:14 +0400

    01 Homework

--- 02_Seminars/01_Seminar/06_Task.py
+++ 02_Seminars/01_Seminar/06_Task.py
@@ -1,3 +1,10 @@
 a = int(input('Введите число: ')) - 1
-result = int(bin(a)) * 5 
-print(result)
+m=a
+p=1
+result=0
+while m>0:
+    result=result+m%2*p
+    p=p*10
+    m=m//2
+
+print(int(result) * 5)
--- 03_HW/01_Seminar_HW/02_Task.py
+++ 03_HW/01_Seminar_HW/02_Task.py
@@ -1,9 +1,9 @@
 # Напишите программу для. проверки истинности утверждения
 #  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 x = True
-y = True
+y = False
 z = True
-if not (x or y or z) == not x and not y and not z:
+if not (x or y or z) == (not x) and (not y) and (not z):
     print('Справедливо')
 else:
     print ('Не справедливо')
